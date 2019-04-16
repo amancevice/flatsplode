1ece0c94ff2b108b5fd046de660add1791c9f290
Alexander Mancevice <amancevice@cargometrics.com>
2019-04-16 17:31:40 -0400

    Update build process

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
+from setuptools import find_packages
+
+
+def requirements(path):
+    with open(path) as req:
+        return [x.strip() for x in req.readlines() if not x.startswith('-')]
+
 
 setup(
     author='amancevice',
     author_email='smallweirdnumber@gmail.com',
     description='Flatten/explode JSON objects.',
+    install_requires=requirements('requirements.txt'),
     name='flatsplode',
-    packages=['flatsplode'],
+    packages=find_packages(exclude=['tests']),
     setup_requires=['setuptools_scm'],
+    tests_require=requirements('requirements-dev.txt'),
     url="http://github.com/amancevice/flatsplode",
     use_scm_version=True,
 )
