269f619659e41ad23b9dcb38d9165438bd98cd94
Alexander Mancevice <amancevice@cargometrics.com>
2019-04-20 09:30:17 -0400

    Relax requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
 
-
-def requirements(path):
-    with open(path) as req:
-        return [x.strip() for x in req.readlines() if not x.startswith('-')]
-
-
 setup(
     author='amancevice',
     author_email='smallweirdnumber@gmail.com',
@@ -15,7 +9,6 @@ setup(
     name='flatsplode',
     packages=find_packages(exclude=['tests']),
     setup_requires=['setuptools_scm'],
-    tests_require=requirements('requirements-dev.txt'),
     url="http://github.com/amancevice/flatsplode",
     use_scm_version=True,
 )
