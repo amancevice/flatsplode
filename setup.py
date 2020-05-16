06353ea4b935bf1835d57ea29df1d39b167f7388
Alexander Mancevice <alexander.mancevice@gmail.com>
2020-05-16 09:35:09 -0400

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
 setup(
     author='amancevice',
     author_email='smallweirdnumber@gmail.com',
     description='Flatten/explode JSON objects.',
     name='flatsplode',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=['tests']),
+    python_requires='>=3.6',
     setup_requires=['setuptools_scm'],
+    tests_require=[
+        'flake8',
+        'pytest',
+        'pytest-cov',
+    ],
     url="http://github.com/amancevice/flatsplode",
     use_scm_version=True,
 )
