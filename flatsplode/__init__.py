f440d3b026b8ef424431f964d10cb852c983026e
Alexander Mancevice <alexander.mancevice@hey.com>
2022-04-27 08:12:19 -0400

    Add __version__

--- flatsplode/__init__.py
+++ flatsplode/__init__.py
@@ -1,6 +1,21 @@
 """
 flatsplode
 """
+from pkg_resources import (get_distribution, DistributionNotFound)
+
 from .flatsplode import explode     # noqa: F401
 from .flatsplode import flatsplode  # noqa: F401
 from .flatsplode import flatten     # noqa: F401
+
+
+def _version():
+    """
+    Helper to get package version.
+    """
+    try:
+        return get_distribution(__name__).version
+    except DistributionNotFound:  # pragma: no cover
+        return None
+
+
+__version__ = _version()
