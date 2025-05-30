7bf91b01689845bc2922e95f9e20520e6af7b482
Alexander Mancevice <alexander.mancevice@hey.com>
2025-05-30 05:33:00 -0400

    ~> v1.0
    add Python v3.13

--- flatsplode/__init__.py
+++ flatsplode/__init__.py
@@ -6,4 +6,4 @@ from .flatsplode import explode  # noqa: F401
 from .flatsplode import flatsplode  # noqa: F401
 from .flatsplode import flatten  # noqa: F401
 
-__version__ = "0.6.0"
+__version__ = "1.0.0"
